772a171ead45f8f4431e4694abe86cc77a5d0aa7
SuperHori <xanlua69@gmail.com>
2022-11-13 21:32:56 +0200

    Done fivem.sh, i really need a beer.

---
+++ cdn/fivem_ignition.sh
@@ -0,0 +1,1 @@
+screen -dm bash -c 'cd /root/fivem-server && bash /root/fivem-server/run.sh'
--- fivem.sh
+++ fivem.sh
@@ -6,6 +6,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 echo "      Thanks For Buying From Ignition."
 echo "With this script you can setup your fivem server"
+echo "      Made By SuperHori♡#6969"
 read -p "Press any key to start installing ..."
 apt -y install sudo
 sudo apt update
@@ -35,3 +36,14 @@ CREATE USER '${db}'@'%' IDENTIFIED BY '${password}';
 GRANT ALL PRIVILEGES ON *.* TO '${db}'@'%';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/naysaku/Fivem-Server-Auto-Setup/main/cdn/fivem.service
+cd /usr/bin/
+wget https://raw.githubusercontent.com/naysaku/Fivem-Server-Auto-Setup/main/cdn/fivem_ignition.sh
+cd
+systemctl enable fivem-server
+txip=`hostname -i`
+echo ""
+echo "Your can connect on your database on https://phpmyadmin.ignitionhost.ro"
+echo "Use your server ip as the server, user and password you set earlier"
+read -p "Server installed, use $txip:40120 to configure your fivem server"
