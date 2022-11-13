558841fc2584e223dc097ce4fee55ad52cf839e3
SuperHori <xanlua69@gmail.com>
2022-11-13 15:43:26 +0200

    Updated fivem.sh

--- fivem.sh
+++ fivem.sh
@@ -14,3 +14,6 @@ mkdir fivem-server
 cd fivem-server
 wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/6019-f14f98f0089b9068916e0fec0180b2ee968d8387/fx.tar.xz
 tar -xvf fx.tar.xz
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+apt update
+apt -y install mariadb-server nginx tar unzip git
